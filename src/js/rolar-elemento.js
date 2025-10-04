d827da1e0d5e3475e4ba21eb5131f355585b4005
jose33-afk <josedev337@gmail.com>
2025-10-04 10:08:41 -0300

    feat: links rapidos funcionando

--- src/js/rolar-elemento.js
+++ src/js/rolar-elemento.js
@@ -1,7 +1,8 @@
 import { pegaElemento } from "./ultils.js";
 
 const listaBnts = pegaElemento('.menu button', 2);
-console.log(listaBnts)
+const listLinks = pegaElemento('.footer-list button', 2);
+console.log(listLinks)
 
 const scrollElement = (elemento) => {
   elemento.scrollIntoView({ //1
@@ -11,6 +12,13 @@ const scrollElement = (elemento) => {
 };
 
 listaBnts.forEach(botao => {
+  botao.addEventListener('click', () => {
+    const posicaoElement = pegaElemento(botao.dataset.posicao);
+    scrollElement(posicaoElement)
+  });
+});
+
+listLinks.forEach(botao => {
   botao.addEventListener('click', () => {
     const posicaoElement = pegaElemento(botao.dataset.posicao);
     scrollElement(posicaoElement)
